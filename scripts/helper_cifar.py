a0ff1145d7b84fe702d70cb997816137452d316c
denisvstepanov <denis_v_stepanov@hotmail.com>
2018-06-09 19:48:47 +0300

    to_image_function changed for 3d tensors

--- scripts/helper_cifar.py
+++ scripts/helper_cifar.py
@@ -28,10 +28,9 @@ def image_normalize(img):
 def to_image_tensor(input_tensor, n_rows=9, n_cols=9, width=64, height=64):
     images_to_plot = n_rows * n_cols
     tensor_to_plot = input_tensor[:images_to_plot, :]
-    image_grid = np.zeros((n_rows * height, n_cols*width))
+    image_grid = np.zeros((3, n_rows * height, n_cols*width))
     for i in range(images_to_plot):
         r = i//n_rows
         c = i%n_cols
-        image_grid[r * height:(r + 1) * height, c * width:(c + 1) * width] = tensor_to_plot[i, :].reshape(width, height)
-    image_grid = np.stack((image_grid, image_grid, image_grid), axis=0)
+        image_grid[:, r * height:(r + 1) * height, c * width:(c + 1) * width] = tensor_to_plot[i, :].reshape(3, width, height)
     return torch.Tensor(image_normalize(image_grid))
